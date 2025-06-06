c5d5280ea504356444bbb73c78c2f2bf3fc63d8b
JSNata <n.kulikouskaya@gmail.com>
2025-06-06 13:05:53 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./dist/**/*.html"],
+    theme: {
+      extend: {
+        container: {
+          center: true,
+          padding: '1rem',
+          screens: {
+            lg: '1280px',
+            xl: '1440px',
+          },
+        },
+      },
+    },
+    plugins: [],
+  }
+  
